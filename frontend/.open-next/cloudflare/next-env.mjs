0a5a7f1043177207b80063a48db67f570c10fb1a
anayet <anayethusenaj@gmail.com>
2026-05-17 11:43:52 +0600

    updated

--- frontend/.open-next/cloudflare/next-env.mjs
+++ frontend/.open-next/cloudflare/next-env.mjs
@@ -1,3 +1,3 @@
-export const production = {"NEXT_PUBLIC_API_URL":"https://boi-pora.onrender.com"};
-export const development = {"NEXT_PUBLIC_API_URL":"http://localhost:4000"};
-export const test = {"NEXT_PUBLIC_API_URL":"http://localhost:4000"};
+export const production = {"NEXT_PUBLIC_API_URL":"https://boi-pora.onrender.com","NEXT_PUBLIC_GET_API_URL":"https://honobackend-production.mdanayet-kdk.workers.dev"};
+export const development = {"NEXT_PUBLIC_API_URL":"http://localhost:4000","NEXT_PUBLIC_GET_API_URL":"http://localhost:8787"};
+export const test = {"NEXT_PUBLIC_API_URL":"http://localhost:4000","NEXT_PUBLIC_GET_API_URL":"http://localhost:8787"};
